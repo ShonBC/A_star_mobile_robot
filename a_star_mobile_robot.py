4ba65ee2d9cca1e4bb49ce98826a7a9a558d3d33
ShonBC <79477080+ShonBC@users.noreply.github.com>
2021-03-31 15:42:14 -0700

    Add files via upload
    Fixed bugs, Need to implement step size and child node orientation tracking.

--- a_star_mobile_robot.py
+++ a_star_mobile_robot.py
@@ -1,19 +1,23 @@
+# ENPM 661 - Planning for Autonomous Robots: 
+# Project 3 Phase 2 - A* on Mobile Robot
+# Shon Cortes, Bo-Shiang Wang
+
 import matplotlib.pyplot as plt
 import numpy as np
-
-show_animation = True
 
 width = 400
 height = 300
 
-class Node: # Class for storing node position, cost to come, and parent index.
-    def __init__(self, x, y, radius, cost, parent_index, theta = 30):
+
+# Class for storing node position, cost to come, parent index, and prev_orientation.
+class Node:
+    def __init__(self, x, y, radius, cost, parent_index, prev_orientation):
         self.x = x
         self.y = y
         self.cost = cost
         self.parent_index = parent_index
         self.radius = radius
-        self.theta = theta
+        self.prev_orientation = prev_orientation
 
 
 def move_check(child_node):  # Check if the move is allowed.
@@ -30,14 +34,16 @@ def move_check(child_node):  # Check if the move is allowed.
     else:
         return True
 
-def obstacles_chk(
-        NODE):  # Check if position is in Robot Adjusted obstacle space. Obstacle space was expanded by a radius of 10 + 5 for clearance for a total of 15. Warning obstacles appear larger than they are.
 
+# Check if position is in Robot Adjusted obstacle space.
+# Obstacle space was expanded by a radius of 10 + 5 for clearance for a total of 15.
+# Warning obstacles appear larger than they are.
+def obstacles_chk(NODE):
     node = [NODE.x, NODE.y]
-    print(node)
+    # print(node)
     # Rectangle
-    if node[1] >= (node[0] * 0.7) + 74.39 - 15 and node[1] <= (node[0] * 0.7) + 98.568 + 15 and node[1] >= (
-            node[0] * -1.428) + 176.554 - 15 and node[1] <= (node[0] * -1.428) + 438.068 + 15:
+    if (node[0] * 0.7) + 74.39 - 15 <= node[1] <= (node[0] * 0.7) + 98.568 + 15 \
+            and (node[0] * -1.428) + 176.554 - 15 <= node[1] <= (node[0] * -1.428) + 438.068 + 15:
         return True
 
     # Circle
@@ -49,27 +55,32 @@ def obstacles_chk(
         return True
 
     # 3 section Rectangular Area
-    elif node[0] >= 200 - 15 and node[0] <= 230 + 15 and node[1] >= 230 - 15 and node[1] <= 280 + 15:  # First section
+    elif 200 - 15 <= node[0] <= 230 + 15 \
+            and 230 - 15 <= node[1] <= 280 + 15:  # First section
         return True
 
     else:
         return False
 
 
-
 def begin():  # Ask for user input of start and goal pos. Start and goal much be positive integers
     while True:
 
-        start_x, start_y = input("Enter starting x and y coordinates separated with a space: ").split()
-        goal_x, goal_y = input("Enter goal x and y coordinates separated with a space: ").split()
-        start_x = int(start_x)
-        start_y = int(start_y)
-        goal_x = int(goal_x)
-        goal_y = int(goal_y)
+        start_x = int(input("Enter starting x coordinate: "))
+        start_y = int(input("Enter starting y coordinate: "))
+        try:
+            start_theta = int(input("Enter starting theta: "))
+        except ValueError:
+            start_theta = 0
 
+        # goal_x, goal_y = input("Enter goal x and y coordinates separated with a space: ").split()
+        goal_x = int(input("Enter goal x coordinate: "))
+        goal_y = int(input("Enter goal y coordinate: "))
+
+        prev_orientation = start_theta
         # Initialize start and goal nodes from node class
-        start_node = Node(start_x, start_y, 15, 0, -1)
-        goal_node = Node(goal_x, goal_y, 15, 0, -1)
+        start_node = Node(start_x, start_y, 15, 0, -1, prev_orientation)
+        goal_node = Node(goal_x, goal_y, 15, 0, -1, 0)
 
         # Check if obstacle
         if obstacles_chk(start_node):
@@ -96,13 +107,17 @@ def euclidean_dist(goal_node, node):
     return dist
 
 
-def motion_model():
-    theta = 30*np.pi/180
-    model = [[np.cos(2*theta), np.sin(2*theta), np.sqrt(np.cos(2*theta)**2+np.sin(2*theta)**2)],
-             [np.cos(theta), np.sin(theta), np.sqrt(np.cos(theta)**2+np.sin(theta)**2)],
-             [1, 0, 1],
-             [np.cos(theta), -np.sin(theta), np.sqrt(np.cos(theta)**2+np.sin(theta)**2)],
-             [np.cos(2*theta), -np.sin(2*theta), np.sqrt(np.cos(2*theta)**2+np.sin(2*theta)**2)]]
+def motion_model(orientation):
+    orientation = np.deg2rad(orientation)
+    theta = np.deg2rad(30)
+    model = [[np.cos(2*theta + orientation), np.sin(2 * theta + orientation), 1],  # 60
+             [np.cos(theta + orientation), np.sin(theta + orientation), 1],  # 30
+             [np.cos(orientation), np.sin(orientation), 1],  # 0
+             [np.cos(-theta + orientation), np.sin(-theta + orientation), 1],  # -30
+             [np.cos(-2 * theta + orientation), np.sin(-2 * theta + orientation), 1]  # -60
+             ]
+
+    # add step size
 
     return model
 
@@ -111,24 +126,29 @@ def a_star(start_node, goal_node):
     # Initialize dictionaries
     path, distance, queue, visited = dict(), dict(), dict(), dict()
 
-    motion = motion_model()  # Initialize action set
-    explored_map = []  # Initialize list of explored positions
     queue[(start_node.x, start_node.y)] = start_node  # Initialize queue with startnode for Dijkstra algorithm.
     distance[(start_node.x, start_node.y)] = 0  # Initialize distance traveled.
 
-    threshold = [.5, 30]
-    vis = []
+    threshold = 0.5
+
+    # Dictionary for orientation
+    orientation_dict = {0: 0, 1: 30, 2: 60, 3: 90,
+                        4: 120, 5: 150, 6: 180, 7: 210,
+                        8: 240, 9: 270, 10: 300, 11: 330, 12: 0}
+
+    # Create V matrix to store the information of the visited nodes.
+    V = np.zeros((int(width/threshold), int(height/threshold), int(360/30)))
 
     while True:  # Start of A star Algorithm.
+        # Find the node in queue with the minimum cost.
+        cur_index = min(queue, key=lambda o: queue[o].cost + euclidean_dist(goal_node, queue[o]))
+        # Assign node in queue with minimum cost to be the current node to be tested.
+        cur = queue[cur_index]
+        orientation = cur.prev_orientation
 
-        cur_index = min(queue, key=lambda o: queue[o].cost + euclidean_dist(goal_node, queue[
-            o]))  # Find the node in queue with the minimum cost.
-        cur = queue[cur_index]  # Assign node in queue with minimum cost to be the current node to be tested.
-
-
-        # if cur.x == goal_node.x and cur.y == goal_node.y:  # If goal node is reached, Break the while loop.
-        if (goal_node.x-threshold[0]) <= cur.x <= (goal_node.x+threshold[0]) \
-                and (goal_node.y-threshold[0]) <= cur.y <= (goal_node.y+threshold[0]):
+        # If goal node is reached, Break the while loop.
+        # Add a threshold(circle) for the goal node
+        if (goal_node.x-cur.x)**2 + (goal_node.y-cur.y)**2 <= 1.5**2:
             # print("Goal!!!")
             goal_node.parent_index = cur.parent_index
             goal_node.cost = cur.cost
@@ -136,29 +156,46 @@ def a_star(start_node, goal_node):
 
         del queue[cur_index]  # Remove the current node from the queue.
         visited[cur_index] = cur  # Add current node to visited list.
-        vis.append((cur.x, cur.y, cur.theta))
 
-        explored_map.append((cur.x, cur.y))  # Add current node to explored map.
+        # Mark 1 for visited nodes in matrix V
+        a = int(round(cur.x)/threshold)
+        b = int(round(cur.y)/threshold)
+        c = orientation_dict[orientation/30]
+        V[a][b][c] = 1
 
-        for i in range(len(motion)):  # Generate childeren of current node based on the action set.
+        motion = motion_model(orientation)
 
-            node = Node(cur.x + round(motion[i][0], 4), cur.y + round(motion[i][1],4), 15, cur.cost + motion[i][2],
-                        cur_index)  # Generate child node
-            node_index = (node.x, node.y)  # Assign child node position
+        # Generate children of current node based on the action set.
+        for i in range(len(motion)):
+            next_x = round(cur.x + motion[i][0], 3)
+            next_y = round(cur.y + motion[i][1], 3)
+            # child_orientation
+            # Initialize action set
+
+            # Generate child node
+            node = Node(next_x, next_y, 15, cur.cost + motion[i][2], cur_index, orientation)
+            # Assign child node position
+            node_index = (node.x, node.y)
 
             if move_check(node):  # Check if child is within the map or in an obstacle.
                 pass
             else:  # If out of bounds or an obstacle, restart loop and choose new node.
                 continue
 
+            a = int(round(node.x))
+            b = int(round(node.y))
+            c = node.prev_orientation
+            # If the next node is already visited, skip it
+            if V[a][b][c] == 1:
+                continue
+            # visualize_Dij(node_index)
             # Visualize motion
-            q1 = plt.quiver(cur.x, cur.y, node.x, node.y, units='xy', scale=1, color= 'r')
-
-            if node_index in visited:  # If the next node is already visited, skip it
-                continue
-
-            if node_check(node, vis, threshold):
-                continue
+            # node_list.append((node.x, node.y))
+            # print(node_list)
+            plt.quiver(cur.x, cur.y, motion[i][0], motion[i][1], units='xy', scale=1, color='r', width = .05)
+            plt.pause(.0001)
+            # if node_index in visited:  # If the next node is already visited, skip it
+            #     continue
 
             if node_index in queue:  # If the child node is already in the queue, compare and update the node's cost and parent as needed.
                 if queue[node_index].cost > node.cost:
@@ -167,27 +204,34 @@ def a_star(start_node, goal_node):
             else:  # Else add child to the queue.
                 queue[node_index] = node
 
+
+    # # Backtrack the path from Goal to Start
+    # path = []
+    # parent_index = goal_node.parent_index
+    # while parent_index != -1:  # Follow the parents from the goal node to the start node and add them to the path list.
+    #     n = visited[parent_index]
+    #     path.append((n.x, n.y))
+    #     parent_index = n.parent_index
+
+    # path = list(reversed(path))  # Reverse the path list to get the path from start node to goal node.
+    # path.append((goal_node.x, goal_node.y))  # Add Goal node to the end of the path list
+    
     # Backtrack the path from Goal to Start
-    path = []
+    path_x, path_y = [goal_node.x], [goal_node.y]
     parent_index = goal_node.parent_index
+    child = visited[parent_index]
+    plt.quiver(child.x, child.y, goal_node.x, goal_node.y, units='xy', scale=1, color='r', width = .05)
     while parent_index != -1:  # Follow the parents from the goal node to the start node and add them to the path list.
         n = visited[parent_index]
-        path.append((n.x, n.y))
+        path_x.append(n.x)
+        path_y.append(n.y)
         parent_index = n.parent_index
 
-    path = list(reversed(path))  # Reverse the path list to get the path from start node to goal node.
-    path.append((goal_node.x, goal_node.y))  # Add Goal node to the end of the path list
-
-
-def node_check(child_node, vis, threshold):
-
-    # visited_pos = vis
-    for i in range(len(vis)):
-        center = vis[i]
-        if center[0] - threshold[0] <= child_node.x <= center[0] + threshold[0] and center[1] - threshold[0] <= child_node.y <= center[1] + threshold[0] and center[2] - threshold[1] <= child_node.theta <= threshold[1] + center[2]:
-            return True
-        else:
-            return False
+    # path = list(reversed(path))  # Reverse the path list to get the path from start node to goal node.
+    # path.append((goal_node.x, goal_node.y))  # Add Goal node to the end of the path list
+    # path_x.append(goal_node.x)
+    # path_y.append(goal_node.y)
+    return path_x, path_y
 
 def main():
 
@@ -205,45 +249,42 @@ def main():
                 ox.append(i)
                 oy.append(j)
 
-            if j >= (i * 0.7) + 74.39 and j <= (i * 0.7) + 98.568 and j >= (i * -1.428) + 176.554 and j <= (
-                    i * -1.428) + 438.068:
+            if (i * 0.7) + 74.39 <= j <= (i * 0.7) + 98.568 \
+                    and (i * -1.428) + 176.554 <= j <= (i * -1.428) + 438.068:
                 ox.append(i)
                 oy.append(j)
 
             # 3 Section Rectangular Area
-            if i >= 200 and i <= 210 and j >= 230 and j <= 280:
+            if 200 <= i <= 210 and 230 <= j <= 280:
                 ox.append(i)
                 oy.append(j)
-            if i >= 210 and i <= 230 and j >= 270 and j <= 280:
+            if 210 <= i <= 230 and 270 <= j <= 280:
                 ox.append(i)
                 oy.append(j)
-            if i >= 210 and i <= 230 and j >= 230 and j <= 240:
+            if 210 <= i <= 230 and 230 <= j <= 240:
                 ox.append(i)
                 oy.append(j)
 
     start_node, goal_node = begin()
-    a_star(start_node, goal_node)  # Call A star algorithm
 
     plt.xlim([0, 400])
     plt.ylim([0, 300])
+    plt.plot(ox, oy, ".k")
     plt.grid(True)
     plt.axis("equal")
-    plt.plot(ox, oy, ".k")
-    
 
-    
-    # plt.plot(rx, ry, "-r")
-    plt.pause(0.001)
-    plt.show()
+    a = [start_node.x, start_node.y]
+    b = [goal_node.x, goal_node.y]
 
+    if a != b:
+        path_x, path_y = a_star(start_node, goal_node)  # Call A star algorithm
+
+        plt.plot(path_x, path_y, "-g")
+        plt.pause(0.0001)
+        plt.show()
+    
+    else:
+        print("Start position equals the goal position.")
 
 if __name__ == '__main__':
     main()
-
-# EASY
-# Display backtracking in differnt color
-# Add User input for theta_start
-
-# Difficult?
-# Why does it take a long time?
-# Ask about tracking robot orientation?
